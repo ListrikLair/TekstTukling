698f8e6fc3ed83fb56a2721bbea8deaecf7cf641
ListrikLair <kevin.strom.trondsetas@gmail.com>
2023-11-24 11:17:08 +0100

    Add project files.

---
+++ TekstTukling/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace TekstTukling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Init();
+            static void Init()
+            {
+                Run();
+            }
+
+            static void Run()
+            {
+                while (true)
+                {
+                    Console.WriteLine("Write some text!");
+                    string input = Console.ReadLine();
+                    Console.WriteLine("Do you want to (1) Reverse your text or (2) Replace all e's with a's");
+                    string reply = Console.ReadLine();
+                    switch (reply)
+                    {
+                        case "1":
+                            string inputReversed = ReverseInput(input);
+                            Console.WriteLine(inputReversed);
+                            break;
+                        case "2":
+                            string inputEWithA = ReplaceEWithA(input);
+                            Console.WriteLine(inputEWithA);
+                            break;
+                        default:
+                            Console.WriteLine("No option Chosen");
+                            break;
+                    }
+                }
+            }
+
+            static string ReverseInput(string input)
+            {
+                char[] inputArray = input.ToCharArray();
+                Array.Reverse(inputArray);
+                return new string(inputArray);
+            }
+
+            static string ReplaceEWithA(string input)
+            {
+                char[] inputArray = input.ToCharArray();
+                for (int i = 0; i < inputArray.Length; i++)
+                {
+                    if (inputArray[i] == 'a')
+                    {
+                        inputArray[i] = 'e';
+                    } else if (inputArray[i] == 'A')
+                    {
+                        inputArray[i] = 'E';
+                    }
+                }
+                return new string(inputArray);
+            }
+        }
+    }
+}
